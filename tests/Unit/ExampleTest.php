a6f884e93b159d169ba4210d55d9c1bc5b0f4f21
varnelle <vpm3@njit.edu>
2018-03-19 18:28:10 -0400

    All other updates

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -16,4 +16,16 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+    public function testBasicTest2()
+    {
+        $this->assertTrue(true);
+    }
+    public function testBasicTest3()
+    {
+        $this->assertTrue(true);
+    }
+    public function testBasicTest4()
+    {
+        $this->assertTrue(true);
+    }
 }
